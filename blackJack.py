f7e0546597b082e655983fbad4d0f3a360ff55a6
Brian Tran <briantran55@vt.edu>
2020-05-04 23:24:48 -0400

    added rules

---
+++ blackJack.py
@@ -0,0 +1,9 @@
+print("*****Welcome To Black Jack*****")
+print("RULES:")
+print("1) The player and the deal both start with two cards.")
+print("2) The player must decide if they want to add another card to their total or stand")
+print("3) This is done by typing 'stand' or 'hit'")
+print("4) If the player goes over 21 they bust")
+print("5) After the player stands, if their total is closer to 21 than the dealer, they win.");
+print("6) Number cards are worth face value, Ace is worth 1,picture cards are worth ten");
+
