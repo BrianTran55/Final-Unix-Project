a8ad5110af579b03c36f66d23f32b37326aa3cc3
Brian Tran <briantran55@vt.edu>
2020-05-05 15:14:16 -0400

    inital while loop for game added

--- blackJack.py
+++ blackJack.py
@@ -4,6 +4,12 @@ print("1) The player and the deal both start with two cards.")
 print("2) The player must decide if they want to add another card to their total or stand")
 print("3) This is done by typing 'stand' or 'hit'")
 print("4) If the player goes over 21 they bust")
-print("5) After the player stands, if their total is closer to 21 than the dealer, they win.");
-print("6) Number cards are worth face value, Ace is worth 1,picture cards are worth ten");
+print("5) After the player stands, if their total is closer to 21 than the dealer, they win.")
+print("6) Number cards are worth face value, Ace is worth 1,picture cards are worth ten")
 
+play = True
+round = 1
+
+while(play):
+	print("Round:",round)
+	play = False
